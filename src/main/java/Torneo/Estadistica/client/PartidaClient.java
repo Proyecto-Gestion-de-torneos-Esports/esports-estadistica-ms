898970f3e2377a8984108ebc1c44558a9cd50a2b
VI-Alumno <godoyamaro59@gmail.com>
2026-05-15 09:56:34 -0400

    correcion en los client

--- src/main/java/Torneo/Estadistica/client/PartidaClient.java
+++ src/main/java/Torneo/Estadistica/client/PartidaClient.java
@@ -5,8 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "partida-service", url = "http://localhost:8004/api/partidas")
+@FeignClient(name = "partidas", url = "http://localhost:8011/api/partidas")
 public interface PartidaClient {
     @GetMapping("/{id}")
-    PartidaResponseDTO obtenerPartidaPorId(@PathVariable("id") Long id);
+    PartidaResponseDTO obtenerPartidaPorId(@PathVariable Long id);
 }
--- src/main/java/Torneo/Estadistica/client/UsuarioClient.java
+++ src/main/java/Torneo/Estadistica/client/UsuarioClient.java
@@ -5,9 +5,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "usuario-service", url = "http://localhost:8001/api/usuarios")
+@FeignClient(name = "usuarios", url = "http://localhost:8014/api/usuarios")
 public interface UsuarioClient {
     @GetMapping("/{id}")
-    UsuarioResponseDTO obtenerUsuarioPorId(@PathVariable("id") Long id);
+    UsuarioResponseDTO obtenerUsuarioPorId(@PathVariable Long id);
 
 }
